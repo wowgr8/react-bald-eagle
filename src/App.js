ff7e55ce30acaded8ebfd5705689da7e6df5bf3e
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-10-18 19:51:23 -0700

    add an empty array variable named todoList. Add unordered list placeholder.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+const todoList = [];
+
 function App() {
   return (
     <div style={{ textAlign: 'center' }}>
       <h1>Todo List</h1>
+      <ul>
+        <li></li>
+      </ul>
     </div>
   );
 }
