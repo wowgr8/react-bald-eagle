4b381c80018fc875bd1ca45bec1a4690fdba5e5c
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-10-24 22:54:54 -0500

    add form tag with label, input and button element tags

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 function AddTodoForm() {
   return (
-    <div>AddTodoForm</div>
+    <form>
+      <label htmlFor="search">Title </label>
+      <input id="todoTitle" type="text"></input>
+      <button type="submit">Add</button>
+    </form>
   )
 }
 
