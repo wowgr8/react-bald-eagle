06b8e1a1d6aa7f91d97335cd359d1889d350f906
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-10-24 22:52:13 -0500

    add AddTodoForm import statement and add it's element tag to multi-line return.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import AddTodoForm from './AddTodoForm';
 import TodoList from './TodoList';
 
 function App() {
   return (
     <div style={{ textAlign: 'center' }}>
       <h1>Todo List</h1>
+      <AddTodoForm />
       <TodoList />
     </div>
   );
