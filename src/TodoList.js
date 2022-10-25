0e266b30be325d68b41f6e04aac30b09992ea9f9
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-10-24 22:00:57 -0500

    add todoList array from app.js

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,20 @@
 import React from 'react'
 
+const todoList = [
+  {
+    id: 1,
+    title: 'Practice React'
+  },
+  {
+    id: 2,
+    title: 'Walk the dog'
+  },
+  {
+    id: 3,
+    title: "Tell dog she's a good girl!"
+  }
+];
+
 function TodoList() {
   return (
     <div>TodoList</div>
