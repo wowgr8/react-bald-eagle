fef5c801bc1448001cf0645504e50f48d7b7d690
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-11-02 16:59:43 -0500

    add onSubmit handler to form tag. Add name attribute to input tag. Add functionality to handleAddTodo function to prevent default behavior and console.log input value, then clear field.

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function AddTodoForm() {
-  const handleAddTodo = (event) => {
+  // const [ titleInput, setTitleInput ] = useState('');
 
+  const handleAddTodo = (event) => {
+    event.preventDefault();
+    const todoTitle = event.target.title.value;
+    console.log(todoTitle);
+    event.target.title.value = "";
+    // setTitleInput('');
   }
 
   return (
-    <form>
+    <form onSubmit={handleAddTodo}>
       <label htmlFor="search">Title </label>
-      <input id="todoTitle" type="text" name="title"></input>
+      <input id="todoTitle" type="text" name="title" />
       <button type="submit">Add</button>
     </form>
   )
