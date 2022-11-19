5adaf46da345fa12e00b6b3d50c253d840be27d0
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-11-18 20:42:59 -0800

    add typo fix - change value attribute to be props.todoTitle instead of useState variable. Update input id and label htmlFor attributes to title for clarity.

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -6,18 +6,24 @@ function AddTodoForm(props) {
   const handleTitleChange = (event) => {    
     const newTodoTitle = event.target.value;
     setTodoTitle(newTodoTitle);
+    console.log(newTodoTitle)
   }
 
   const handleAddTodo = (event) => {
+    console.log("test00")
     event.preventDefault();
-    props.onAddTodo(todoTitle)
-    event.target.title.value = "";
+    console.log("test0")
+    props.onAddTodo({title: todoTitle, id: Date.now()});
+    console.log("test1")
+    
+    setTodoTitle("");
+    console.log("test2")
   }
 
   return (
     <form onSubmit={handleAddTodo}>
-      <label htmlFor="todoTitle">Title </label>
-      <input id="todoTitle" type="text" name="title" value={todoTitle} onChange={handleTitleChange}/>
+      <label htmlFor="title">Title </label>
+      <input id="title" type="text" name="title" value={props.todoTitle} onChange={handleTitleChange}/>
       <button type="submit">Add</button>
     </form>
   )
