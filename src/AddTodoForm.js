bc2ab215571ec7ef9210606f3529070153a1b316
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-11-18 19:28:24 -0800

    add handleTitleChange function that stores event value in newTodoTitle variable and pass variable into setTodoTitle state setter function.

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -3,9 +3,15 @@ import React, { useState } from 'react'
 function AddTodoForm(props) {
   const [ todoTitle, setTodoTitle ] = useState();
 
+  const handleTitleChange = (event) => {    
+    const newTodoTitle = event.target.value;
+    setTodoTitle(newTodoTitle);
+    console.log(newTodoTitle);
+  }
+
   const handleAddTodo = (event) => {
     event.preventDefault();
-    const todoTitle = event.target.title.value;
+    const todoTitle = event.target.todoTitle.value;
     props.onAddTodo(todoTitle)
     event.target.title.value = "";
   }
