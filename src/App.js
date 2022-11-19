082ad1fa0a35395115b6b029f8cefa1b50215017
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-11-18 19:52:45 -0800

    remove newTodo useState variable, add addTodo() that takes in newTodo parameter and spreads parameter into todoList via setTodoList(). Refactor onAddTodo prop to invoke addTodo function.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import AddTodoForm from './AddTodoForm';
 import TodoList from './TodoList';
 
 function App() {
-  const [ newTodo, setNewTodo ] = useState();
   const [ todoList, setTodoList ] = useState([]);
+
+  const addTodo = (newTodo) => {
+    setTodoList(todoList, ...newTodo)
+  }
 
   return (
     <div style={{ textAlign: 'center' }}>
       <h1>Todo List</h1>
-      <AddTodoForm onAddTodo={setNewTodo} />
-      <p>{newTodo}</p>
+      <AddTodoForm onAddTodo={addTodo} />
       <TodoList todoList={todoList} />
     </div>
   );
