e2123f4d6f6224da1089dab4b1ee08aee13c4e89
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-11-22 15:49:58 -0800

    add useSemiPersistentState custom hook - moved previous useState and useEffect hooks into it. Update App() component to use useSemiPersistentState custom hook.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React, { useState, useEffect } from 'react';
 import AddTodoForm from './AddTodoForm';
 import TodoList from './TodoList';
 
-function App() {
+const useSemiPersistentState = () => {
   const [ todoList, setTodoList ] = useState(JSON.parse(localStorage.getItem("savedTodoList")));
-
+  
   useEffect(() => {
     localStorage.setItem("savedTodoList", JSON.stringify(todoList));
-  }, [todoList])
-  
+  }, [todoList]);
+
+  return [ todoList, setTodoList ];
+}
+
+function App() {
+  const [ todoList, setTodoList ] = useSemiPersistentState();
 
   const addTodo = (newTodo) => {
     setTodoList([...todoList, newTodo])
