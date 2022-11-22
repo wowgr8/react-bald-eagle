2fd6ccfd6ba583f818f84422bef75fe6bbcb542b
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-11-22 15:31:10 -0800

    add useEffect() that saves search input to localStorage. The search object is given a "savedTodoList" flexible key and stringified. todoList set as dependency.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AddTodoForm from './AddTodoForm';
 import TodoList from './TodoList';
 
 function App() {
   const [ todoList, setTodoList ] = useState([]);
+
+  useEffect(() => {
+    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+  }, [todoList])
+  
 
   const addTodo = (newTodo) => {
     setTodoList([...todoList, newTodo])
