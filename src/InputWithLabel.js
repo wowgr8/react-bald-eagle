723a140ed7ac39d8379dca1831800795f81205e8
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-12-06 14:40:20 -0800

    replace label prop with children prop -  for component composition.

--- src/InputWithLabel.js
+++ src/InputWithLabel.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function InputWithLabel({ todoTitle, handleTitleChange, label }) {
+function InputWithLabel({ todoTitle, handleTitleChange, children }) {
   return (
     <>
-      <label htmlFor="todoTitle">{label} </label>
+      <label htmlFor="todoTitle">{children} </label>
       <input id="todoTitle" type="text" name="title" value={todoTitle} onChange={handleTitleChange}/>
     </>
   )
