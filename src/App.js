c382a7fcacee290f7fd6ccf8af4736d97bd916a9
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-01-17 22:01:51 -0800

    add import statement for react-router-dom, add Routes and route elements to multi-line return as well as "New Todo List" Route.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import AddTodoForm from './AddTodoForm';
 import TodoList from './TodoList';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   const [ todoList, setTodoList ] = useState([]);
@@ -38,14 +39,21 @@ function App() {
   }
 
   return (
-    <>
-      <h1>Todo List</h1>
-      <AddTodoForm onAddTodo={addTodo} />
-      {isLoading 
-        ? (<p>Loading...</p>)
-        : (<TodoList todoList={todoList} onRemoveTodo={removeTodo} />)
-      }
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' exact element={
+          <div>
+            <h1>Todo List</h1>
+            <AddTodoForm onAddTodo={addTodo} />
+            {isLoading 
+              ? (<p>Loading...</p>)
+              : (<TodoList todoList={todoList} onRemoveTodo={removeTodo} />)
+            }
+          </div>
+        } />
+        <Route path='/new' exact element={<h1>New Todo List</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
