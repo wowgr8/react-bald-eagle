ccbb1ab769bf07361c7e6957db5e10309e7aea3e
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-01-30 19:18:27 -0800

    add import for CSS module and apply styling to elements - WIP

--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { ReactComponent as RedCheckMark } from './SVG/done-checkmark.svg';
+import styles from './CSS/TodoListItem.module.css';
 
 function TodoListItem({ fields, onRemoveTodo, id }) {
+  console.log(fields)
+  console.log(fields.Title)
   return (
-    <>
-      <li>{fields.Title}</li>
-      <button type="button" onClick={() => onRemoveTodo(id)} >
-        Remove
+    <div >
+      <li className={styles.ul__align} >{fields.Title} </li>
+      <button type="button" onClick={() => onRemoveTodo(id)} className={styles.btn__align} >
+        <RedCheckMark height="18px" width="18px" />
       </button>
-    </>
+    </div>
   )
 }
 
