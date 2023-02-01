b4680384c59509d0c6bfe954ebfc26ea90895101
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-01-31 16:51:03 -0800

    add empty string to todoTitle default state. Add array encapsulating onAddTodo object. Remove onAddTodo from todoTitle prop - was never needed there.

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -4,7 +4,7 @@ import styles from './CSS/AddTodoForm.module.css';
 
 
 function AddTodoForm({ onAddTodo }) {
-  const [ todoTitle, setTodoTitle ] = useState();
+  const [ todoTitle, setTodoTitle ] = useState('');
 
   const handleTitleChange = (event) => {    
     const newTodoTitle = event.target.value;
@@ -13,14 +13,14 @@ function AddTodoForm({ onAddTodo }) {
 
   const handleAddTodo = (event) => {
     event.preventDefault();
-    onAddTodo({ id: Date.now(), title: todoTitle });
+    onAddTodo([{ id: Date.now(), title: todoTitle }]); 
     setTodoTitle("");
   }
 
   return (
     <form onSubmit={handleAddTodo} className={styles.titleFont}> 
       <InputWithLabel 
-        todoTitle={onAddTodo.todoTitle} 
+        todoTitle={todoTitle} 
         handleTitleChange={handleTitleChange}
       >
         Title:
