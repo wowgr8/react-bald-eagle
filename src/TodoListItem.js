dc34424136db6bbdaf1bfcccabee1a5386f25e85
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-01-31 19:18:37 -0800

    remove console.logs

--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -3,11 +3,9 @@ import { ReactComponent as RedCheckMark } from './SVG/done-checkmark.svg';
 import styles from './CSS/TodoListItem.module.css';
 
 function TodoListItem({ fields, onRemoveTodo, id }) {
-  console.log(fields)
-  console.log(fields.Title)
   return (
     <div >
-      <li className={styles.ul__align} >{fields.Title} </li>
+      <li className={styles.ul__align} >{fields.Title} </li> 
       <button type="button" onClick={() => onRemoveTodo(id)} className={styles.btn__align} >
         <RedCheckMark height="18px" width="18px" />
       </button>
